2debbaea9df5ae72b3e9cfc79757ee37607c5563
Roman <romario123321@yandex.ru>
2025-02-27 13:34:36 +0300

    Enhance process monitoring script with improved logging and timeout handling
    - Update .env-example to include TIMEOUT variable
    - Modify process_alarm.sh to:
      * Create log file if it doesn't exist
      * Add timeout to curl request
      * Implement process restart detection
      * Improve temporary file handling
    - Update deployment workflow to use new LOG_FILE_PATH variable

--- process_alarm.sh
+++ process_alarm.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
-LOG_FILE=$(grep 'LOG_FILE=' .env | cut -d'=' -f2)
+LOG_FILE_PATH=$(grep 'LOG_FILE=' .env | cut -d'=' -f2)
 API_URL=$(grep 'API_URL=' .env | cut -d'=' -f2)
 PROCESS_NAME=$(grep 'PROCESS_NAME=' .env | cut -d'=' -f2)
 ACCESS_TOKEN=$(grep 'ACCESS_TOKEN=' .env | cut -d'=' -f2)
+TIMEOUT=$(grep 'TIMEOUT=' .env | cut -d'=' -f2)
 
+# Создаем лог-файл если он не существует и устанавливаем права
+if [ ! -f "$LOG_FILE_PATH" ]; then
+    sudo touch "$LOG_FILE_PATH"
+    sudo chmod 666 "$LOG_FILE_PATH"
+fi
 
 if pgrep -x "$PROCESS_NAME" > /dev/null; then
-    if [ ! -f /tmp/${PROCESS_NAME}_process_running ]; then
-        echo "$(date): Процесс '$PROCESS_NAME' был запущен." >> "$LOG_FILE"
+    # Проверка на перезапуск
+    if [ -f /tmp/${PROCESS_NAME}_process_stopped ]; then
+        echo "$(date): Процесс '$PROCESS_NAME' был перезапущен." >> "$LOG_FILE_PATH"
+        rm /tmp/${PROCESS_NAME}_process_stopped
     fi
-
-    touch /tmp/"${PROCESS_NAME}_process_running"
+    
+    if [ ! -f /tmp/${PROCESS_NAME}_process_running ]; then
+        echo "$(date): Процесс '$PROCESS_NAME' был запущен." >> "$LOG_FILE_PATH"
+        touch /tmp/"${PROCESS_NAME}_process_running"
+    fi
 
     JSON_DATA=$(cat <<EOF
 {   
-    "server": "$(hostname -I | awk '{print $1}')"
+    "server": "$(hostname -I | awk '{print $1}')",
     "timestamp": "$(date)",
     "process_name": "$PROCESS_NAME",
     "status": "running",
@@ -23,18 +34,18 @@ if pgrep -x "$PROCESS_NAME" > /dev/null; then
 EOF
 )
 
-
-    RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X POST "$API_URL" \
+    RESPONSE=$(curl -s -o --max-time $TIMEOUT /dev/null -w "%{http_code}" -X POST "$API_URL" \
         -H "Content-Type: application/json" \
         -H "Authorization: Bearer $ACCESS_TOKEN" \
         -d "$JSON_DATA")
 
     if [ "$RESPONSE" -ne 200 ]; then
-        echo "$(date): Сервер мониторинга недоступен, код ответа: $RESPONSE" >> "$LOG_FILE"
+        echo "$(date): Сервер мониторинга недоступен, код ответа: $RESPONSE" >> "$LOG_FILE_PATH"
     fi
 else
     if [ -f /tmp/${PROCESS_NAME}_process_running ]; then
-        echo "$(date): Процесс '$PROCESS_NAME' остановлен." >> "$LOG_FILE"
-        rm /tmp/test_process_running
+        echo "$(date): Процесс '$PROCESS_NAME' остановлен." >> "$LOG_FILE_PATH"
+        rm /tmp/${PROCESS_NAME}_process_running
+        touch /tmp/${PROCESS_NAME}_process_stopped
     fi
 fi
